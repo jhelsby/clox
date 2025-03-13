e887993c3ec276bc1b96ebbd37bf18296eaaa75e
jlh <11036537+jhels@users.noreply.github.com>
2025-03-13 16:59:39 +0000

    Added temporary test code to create, modify, and free a chunk

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, const char* argv[]) {
+
+    // Temporary testing code.
+    // Manually create a chunk, append one 
+    // OP_RETURN instruction to it, and free it.
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
     return 0;
 }
