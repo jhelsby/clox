c0d65fffee155d623a19b50fe1344627151d48b2
jhels <11036537+jhels@users.noreply.github.com>
2025-03-21 16:46:49 +0000

    Added REPL and file reading to main.c

--- main.c
+++ main.c
@@ -1,49 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+
+        // Exit gracefully in case of an error or EOF character.
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+
+    if (file == NULL) {
+        fprintf(stderr, "Couldn't open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    // Determine the file length.
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    // Allocate enough memory to store the whole file.
+    char* buffer = (char*);
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    malloc(fileSize + 1);
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+
+    // Handle the read failing.
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
 int main(int argc, const char* argv[]) {
     // The VM is used throughout the interpreter's lifetime.
     initVM();
 
-    // Temporary testing code.
-    // Currently testing the binary arithmetic operations.
-    // Specifically, the expression -((1.2 + 3.4) / 5.6).
-    Chunk chunk;
-    initChunk(&chunk);
-
-    int constant = addConstant(&chunk, 1.2);
-
-    // Pass in arbitrary line number (123) for testing.
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    writeChunk(&chunk, OP_RETURN, 123);
-
-    // Disassemble the test chunk and
-    // print its contents.
-    disassembleChunk(&chunk, "test chunk");
-
-    // Tell the VM to interpret the current bytecode chunk.
-    interpret(&chunk);
+    // Run the REPL or a Lox file depending on the number of arguments.
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: clox [path]\n");
+        exit(64);
+    }
 
     freeVM();
-    freeChunk(&chunk);
-
     return 0;
 }
