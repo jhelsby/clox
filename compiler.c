443b4d58ea9d513c79ff1a3c354b4ec9082323bd
jhels <11036537+jhels@users.noreply.github.com>
2025-03-23 09:09:15 +0000

    Set up compiler boilerplate to prepare for Pratt Parsing

--- compiler.c
+++ compiler.c
@@ -4,27 +4,9 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
+void compile(const char* source, Chunk* chunk) {
   initScanner(source);
-
-  // Temporary testing code to scan through
-  // Lox code. Ask the scanner for tokens
-  // until an EOF character. Print these tokens.
-  int line = -1;
-  for (;;) {
-    Token token = scanToken();
-    // If the token is on a new line of code,
-    // print the line number.
-    if (token.line != line) {
-      printf("%4d ", token.line);
-      line = token.line;
-    } else {
-      printf("   | ");
-    }
-
-    // Print the raw index of the token type, and the token itself.
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-    if (token.type == TOKEN_EOF) break;
-  }
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expect end of expression.");
 }
