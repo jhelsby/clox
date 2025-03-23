d36c894f6a0040012d310d8417313239fc59fbe3
jhels <11036537+jhels@users.noreply.github.com>
2025-03-23 10:33:54 +0000

    Added parsing functionality to the compiler

--- compiler.c
+++ compiler.c
@@ -1,12 +1,96 @@
+// Single-pass compiler. Parses source code,
+// producing parts of an AST as needed,
+// and outputting bytecode as it goes.
+
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+
+  // Flag to avoid error cascades.
+  // Tracks whether errors have occurred,
+  // so we only report the first error.
+  bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token* token, const char* message) {
+  // Only report the first error we find.
+  // Avoids reporting error cascades.
+  if (parser.panicMode) return;
+  parser.panicMode = true;
+
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_EOF) {
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // Nothing.
+  } else {
+    // Show the lexeme (if human-readable).
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+// Report an error at the current token.
+static void errorAtCurrent(const char* message) {
+  errorAt(&parser.current, message);
+}
+
+// Advance through the token stream,
+// reporting errors as they are found.
+// Only pass valid tokens to the
+// rest of the parser.
+static void advance() {
+  // Store the old token, so we can retrieve
+  // its lexeme when matching a token.
+  parser.previous = parser.current;
+
+  // Only break the loop when
+  // non-error tokens are found.
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) break;
+
+    // Report any errors.
+    errorAtCurrent(parser.current.start);
+  }
+}
+
+// Advance through the token stream if
+// the token matches an expected type.
+// Otherwise, report an error.
+// Allows us to identify syntax errors.
+static void consume(TokenType type, const char* message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
 void compile(const char* source, Chunk* chunk) {
   initScanner(source);
+
+  parser.hadError = false;
+  parser.panicMode = false;
+
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+
+  // Indicate whether the compilation was successful.
+  return !parser.hadError;
 }
